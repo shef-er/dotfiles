cebfc55e2d3aa6fd9884146171db8065f552773c
Ernest Shefer <shef-er@ya.ru>
2023-03-14 04:07:00 +0500

    Update zsh configuration

--- home/.config/zsh/rc.d/00-history.zsh
+++ home/.config/zsh/rc.d/00-history.zsh
@@ -7,6 +7,6 @@
 
 setopt appendhistory histignoredups histignorespace
 
-export HISTFILE="$HOME/.local/state/zsh_history"
+export HISTFILE="$XDG_STATE_HOME/zsh_history"
 export HISTSIZE=1000
 export SAVEHIST=10000
