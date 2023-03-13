612c0111e9df17fa10945592d78174060c726309
Ernest Shefer <shef-er@ya.ru>
2023-03-14 04:26:49 +0500

    Update zsh configuration

--- home/.config/zsh/env.d/00-envvars.zsh
+++ home/.config/zsh/env.d/00-envvars.zsh
@@ -4,7 +4,7 @@ export EDITOR=nano
 export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
 
 WINEPREFIXES_DIR="$XDG_DATA_HOME/wineprefixes"
-mkdir -p "$WINEPREFIXES_DIR"
+[ ! -d "$WINEPREFIXES_DIR" ] && mkdir -p "$WINEPREFIXES_DIR"
 export WINEPREFIX="$WINEPREFIXES_DIR/default"
 
 export QT_QPA_PLATFORM=wayland
