fa1f85a158a8b7b9fac6bcbac82827a7825a6426
Ernest Shefer <shef-er@ya.ru>
2023-03-15 01:30:52 +0500

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "$0 <source_dir> <destination_dir>"
+usage() {
+    echo "Usage: $0 <source_dir> <destination_dir>"
     exit 1
-fi
+}
+
+[ -z "$1" ] || [ -z "$2" ] && usage
 
 for path in $(find "$1" -mindepth 1 -type f); do
     src_path="$(realpath "$path")"
