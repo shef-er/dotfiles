22c8ebec8ed5803f8beb0e76e886fab3c1103b64
Ernest Shefer <shef-er@ya.ru>
2023-03-16 03:05:07 +0500

    Update link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -7,11 +7,16 @@ usage() {
 
 [ -z "$1" ] || [ -z "$2" ] && usage
 
+STATE_FILE="$HOME"/.local/state/dotfiles
+
+mkdir -p "$(dirname "$STATE_FILE")"
+
 for path in $(find "$1" -mindepth 1 -type f); do
     src_path="$(realpath "$path")"
     dst_path="$(realpath $2)/${path#"$1/"}"
 
     mkdir -p "$(dirname "$dst_path")"
     ln -vsfT "$src_path" "$dst_path"
-done
 
+    echo "$dst_path" >> "$STATE_FILE"
+done
