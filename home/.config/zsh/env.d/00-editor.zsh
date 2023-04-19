faa976d7c1ec5ba11dc3873a470c8b426afb1b8d
Ernest Shefer <shef-er@ya.ru>
2023-04-19 14:49:29 +0500

    Update zsh config

--- home/.config/zsh/env.d/00-editor.zsh
+++
@@ -1,2 +0,0 @@
-export VISUAL=nano
-export EDITOR=nano
--- home/.config/zsh/env.d/00-wine.zsh
+++ home/.config/zsh/env.d/00-wine.zsh
@@ -1,3 +1,9 @@
+#           _
+# __      _(_)_ __   ___
+# \ \ /\ / / | '_ \ / _ \
+#  \ V  V /| | | | |  __/
+#   \_/\_/ |_|_| |_|\___|
+
 WINEPREFIXES_DIR="$XDG_DATA_HOME"/wineprefixes
 [ ! -d "$WINEPREFIXES_DIR" ] && mkdir -p "$WINEPREFIXES_DIR"
 
