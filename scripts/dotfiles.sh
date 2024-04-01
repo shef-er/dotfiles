df09cd0763a047f40262949ba701818cbc0e3923
Ernest Shefer <shef-er@ya.ru>
2024-04-01 12:36:55 +0500

    Update dotfiles.sh

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -3,7 +3,7 @@
 readonly STATE_FILE="$HOME"/.local/state/dotfiles
 
 ## Yes, I know about GNU Stow, just want to use my own solution
-usage() {
+dotfiles::usage() {
     echo
     echo "Usage: $0 [COMMAND]"
     echo
@@ -12,54 +12,57 @@ usage() {
     echo "  unlink                            Remove symbolic links to dotfiles"
 }
 
-exit_with_error() {
+dotfiles::exit_with_error() {
     echo -en '\e[31mERROR:\e[0m '
     echo "${1:-}"
     echo 'Aborting...'
     exit 1
 }
 
-echo_unlink() {
+dotfiles::echo_unlink() {
     echo -en '\e[33mUNLINK:\e[0m '
     echo "$1"
 }
 
-echo_link() {
+dotfiles::echo_link() {
     echo -en '\e[32mLINK:\e[0m '
     echo "$1"
 }
 
-unlink() {
+dotfiles::unlink() {
     [ -r "$STATE_FILE" ] || exit 0
 
     links="$(< "$STATE_FILE")"
 
+    IFS=$'\n'
     for link in $links; do
         if [ -e "$link" ] && [ ! -L "$link" ]; then
-            exit_with_error "Cannot remove link '$link': File is not a link"
+            dotfiles::exit_with_error "Cannot remove link '$link': File is not a link"
         fi
     done
 
     for link in $links; do
-        rm -f "$link" && echo_unlink "'$link'"
+        rm -f "$link" && dotfiles::echo_unlink "'$link'"
     done
+    unset IFS
 
     rm -f "$STATE_FILE"
 }
 
-link() {
-    [ -r "$STATE_FILE" ] && unlink
+dotfiles::link() {
+    [ -r "$STATE_FILE" ] && dotfiles::unlink
 
     target_dir="$(realpath "$1")"
     destination_dir="$(realpath $2)"
 
-    targets="$(find "$target_dir" -mindepth 1 -type f)"
+    targets="$(find "$target_dir" -mindepth 1 -type f -print)"
 
+    IFS=$'\n'
     for target in $targets; do
         link="$destination_dir${target#"$target_dir"}"
 
         if [ -e "$link" ]; then
-            exit_with_error "Cannot create link '$link': File already exists"
+            dotfiles::exit_with_error "Cannot create link '$link': File already exists"
         fi
     done
 
@@ -70,22 +73,23 @@ link() {
 
         mkdir -p "$(dirname "$link")" \
             && ln -sT "$target" "$link" \
-            && echo_link "'$link' -> '$target'"
+            && dotfiles::echo_link "'$link' -> '$target'"
 
         echo "$link" >> "$STATE_FILE"
     done
+    unset IFS
 }
 
 case "$1" in
     "link")
-        [ -z "$2" ] || [ ! -d "$2" ] || [ -z "$3" ] || [ ! -d "$3" ] && usage && exit 1
-        link "$2" "$3"
+        [ -z "$2" ] || [ ! -d "$2" ] || [ -z "$3" ] || [ ! -d "$3" ] && dotfiles::usage && exit 1
+        dotfiles::link "$2" "$3"
         ;;
     "unlink")
-        unlink
+        dotfiles::unlink
         ;;
     *)
-        usage
+        dotfiles::usage
         ;;
 esac
  
