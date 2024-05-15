44aa4daba11a4420e8e69b7603607c225b8d3aee
Ernest Shefer <shef-er@ya.ru>
2024-05-16 01:07:39 +0500

    Update dotfiles script

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -2,6 +2,21 @@
 
 readonly STATE_FILE="$HOME"/.local/state/dotfiles
 
+function dotfiles::main {
+    case "$1" in
+        "link")
+            [ -z "$2" ] || [ ! -d "$2" ] || [ -z "$3" ] || [ ! -d "$3" ] && dotfiles::usage && exit 1
+            dotfiles::link "$2" "$3"
+            ;;
+        "unlink")
+            dotfiles::unlink
+            ;;
+        *)
+            dotfiles::usage
+            ;;
+    esac
+}
+
 ## Yes, I know about GNU Stow, just want to use my own solution
 dotfiles::usage() {
     echo
@@ -10,43 +25,6 @@ dotfiles::usage() {
     echo "Commands:"
     echo "  link SOURCE DESTINATION           Create symbolic links in DESTINATION dir to dotfiles in SOURCE dir"
     echo "  unlink                            Remove symbolic links to dotfiles"
-}
-
-dotfiles::exit_with_error() {
-    echo -en '\e[31mERROR:\e[0m '
-    echo "${1:-}"
-    echo 'Aborting...'
-    exit 1
-}
-
-dotfiles::echo_unlink() {
-    echo -en '\e[33mUNLINK:\e[0m '
-    echo "$1"
-}
-
-dotfiles::echo_link() {
-    echo -en '\e[32mLINK:\e[0m '
-    echo "$1"
-}
-
-dotfiles::unlink() {
-    [ -r "$STATE_FILE" ] || exit 0
-
-    links="$(< "$STATE_FILE")"
-
-    IFS=$'\n'
-    for link in $links; do
-        if [ -e "$link" ] && [ ! -L "$link" ]; then
-            dotfiles::exit_with_error "Cannot remove link '$link': File is not a link"
-        fi
-    done
-
-    for link in $links; do
-        rm -f "$link" && dotfiles::echo_unlink "'$link'"
-    done
-    unset IFS
-
-    rm -f "$STATE_FILE"
 }
 
 dotfiles::link() {
@@ -80,16 +58,42 @@ dotfiles::link() {
     unset IFS
 }
 
-case "$1" in
-    "link")
-        [ -z "$2" ] || [ ! -d "$2" ] || [ -z "$3" ] || [ ! -d "$3" ] && dotfiles::usage && exit 1
-        dotfiles::link "$2" "$3"
-        ;;
-    "unlink")
-        dotfiles::unlink
-        ;;
-    *)
-        dotfiles::usage
-        ;;
-esac
+dotfiles::unlink() {
+    [ -r "$STATE_FILE" ] || exit 0
+
+    links="$(< "$STATE_FILE")"
+
+    IFS=$'\n'
+    for link in $links; do
+        if [ -e "$link" ] && [ ! -L "$link" ]; then
+            dotfiles::exit_with_error "Cannot remove link '$link': File is not a link"
+        fi
+    done
+
+    for link in $links; do
+        rm -f "$link" && dotfiles::echo_unlink "'$link'"
+    done
+    unset IFS
+
+    rm -f "$STATE_FILE"
+}
+
+dotfiles::echo_link() {
+    echo -en '\e[32mLINK:\e[0m '
+    echo "$1"
+}
+
+dotfiles::echo_unlink() {
+    echo -en '\e[33mUNLINK:\e[0m '
+    echo "$1"
+}
+
+dotfiles::exit_with_error() {
+    echo -en '\e[31mERROR:\e[0m '
+    echo "${1:-}"
+    echo 'Aborting...'
+    exit 1
+}
+
+dotfiles::main "$@"
  
